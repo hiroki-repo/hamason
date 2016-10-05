cea76d42e5d025cacb22c92ae638ef142156d729
hiroki-repo <hiroki7v11@gmail.com>
2016-10-05 18:28:13 +0900

    Create index.php

---
+++ cloud/index.php
@@ -0,0 +1,49 @@
+
+<!doctype html>
+<html>
+<head>
+	<title>main menu</title>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+	<div style="width: 90%; max-width: 800px; margin: 0 auto;">
+		<h1>SolusVM-Status</h1>
+		<hr/>
+		
+				
+				
+			<h3>Credit</h3>
+			0.00$			
+			<h3>Status</h3>
+			Active
+		
+			<h3>Credit usage <small>100% of 20%</small></h3>
+			<div class="progress">
+				<div class="progress-bar" role="progressbar" aria-valuenow="percentused" aria-valuemin="0" aria-valuemax="100" style="width: 20%;">
+					20%
+				</div>
+			</div>
+			
+			
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Actions</h3>
+				</div>
+				
+				<div class="panel-body">
+									<a class="btn btn-primary btn-block" href="ec2" role="button">Use a EC2</a>
+					<a class="btn btn-primary btn-block" href="storage" role="button">Use a Storage</a>
+					<a class="btn btn-default btn-block" href="?action=info" role="button">Reload the page</a>
+								</div>
+			</div>
+		
+				
+				
+		
+				<footer style="margin: 20px 0; text-align: center;">
+			<a href="https://github.com/cuonic/SolusVM-Status" target="_blank">SolusVM-Status</a> by Cuonic
+		</footer>
+	</div>
+</body>
+</html>
