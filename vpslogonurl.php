c0e2f47e1a3248d61ab94fb5772e2c343534f91f
hiroki-repo <hiroki7v11@gmail.com>
2016-10-05 21:09:31 +0900

    Update vpslogonurl.php

--- vpslogonurl.php
+++ vpslogonurl.php
@@ -1,4 +1,5 @@
 <?php
 setcookie("vpsuser",$_POST["myvpsset"].$_GET["myvpsset"]); 
+setcookie("customermail","hiroki-repo@hws.hamason.tld");
 ?>
 <meta http-equiv="refresh" content="0;URL=vpstp.php">
