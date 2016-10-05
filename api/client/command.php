421cf652fd82099daa51ee04decc7e95d5d752b4
hiroki-repo <hiroki7v11@gmail.com>
2016-10-05 18:21:06 +0900

    Update command.php

--- api/client/command.php
+++ api/client/command.php
@@ -1,9 +1,12 @@
 <?php
-function getcloud($option,$postoption,$postoptiontf){
-$URL = "https://beescale.com/".$option;
+function getcloud($option,$postoption,$postoptiontf,$demomode,$gusername,$gpassword){
+if($demomode=="true"){$URL = "https://beescale.com/api/".$option;
 
 $USERNAME = "a3d4que6lv4j@sute.jp";
-$PASSWORD = "611c2979bf7913df34a0f6d7890cdaf0d60d6931";
+$PASSWORD = "611c2979bf7913df34a0f6d7890cdaf0d60d6931";}else{$URL = $option;
+
+$USERNAME = $gusername;
+$PASSWORD = $gpassword;}
  
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $URL);
@@ -15,21 +18,40 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $postoption);}
 $buf = curl_exec($ch);
 curl_close($ch);
  
-echo str_replace("https://beescale.com/api","https://agile-reef-17573.herokuapp.com/api",$buf);
-return simplexml_load_string($buf);
+//echo str_replace("https://beescale.com/api","https://agile-reef-17573.herokuapp.com/api",$buf);
+
+$buf2=simplexml_load_string($buf);
+$json = json_encode($buf2);
+$xmlar=json_decode($json,TRUE);
+
+return simplexml_load_string($xmlar);
 }
+
+$instances=getcloud("instances","","false","false","","");
+
+$xmlars=getcloud("instances","","false","false","","");
+$xmlars1=$xmlars["actions"];
+$xmlars2=$xmlars1["link"];
+$xmlars3=$xmlars2["0"];
+$xmlars4=$xmlars3["@attributes"];
+$boot=str_replace("https://beescale.com/api/","",$xmlars3["@attributes"]);
+
+if($instances["stats"]=="pending"){$vmstatus="offline";}else{if($instances["stats"]=="poweroff"){$vmstatus="offline";}else{$vmstatus="online";}}
 
 if($_POST["key"]=="test"){
 if($_POST["hash"]=="test"){
-if($_POST["status"]=="true"){echo "<vmstat>online</vmstat>";}
+if($_POST["status"]=="true"){echo "<vmstat>".$vmstatus."</vmstat>";}
 if($_POST["action"]=="status"){
-echo "<status>success</status><statusmsg>online</statusmsg>";}
+echo "<status>success</status><statusmsg>".$vmstatus."</statusmsg>";}
+if($_POST["action"]=="make"){
+echo "<status>success</status><statusmsg>".$vmstatus."</statusmsg>";}
 if($_POST["action"]=="shutdown"){
 echo "<status>success</status><statusmsg>shutdown</statusmsg>";}
 if($_POST["action"]=="reboot"){
 echo "<status>success</status><statusmsg>rebooted</statusmsg>";}
 if($_POST["action"]=="boot"){
+$instances=getcloud($boot,"","true","false","","");
 echo "<status>success</status><statusmsg>booted</statusmsg>";}
-if($_POST["action"]=="info"){echo "<ipaddr>123.123.123.123,122.122.122.122,111.111.111.111</ipaddr><hdd>10240000000,30720000,993280000,percentused</hdd><mem>1024000000,1024000,1022976000,percentused</mem><bw>10240000000000,1024000000,1022976000000,percentused</bw><hostname>hirokivps</hostname><ipaddress>37.59.241.174</ipaddress>";}
+if($_POST["action"]=="info"){echo "<ipaddr>123.123.123.123,122.122.122.122,111.111.111.111</ipaddr><hdd>10240000000,30720000,993280000,percentused</hdd><mem>1024000000,1024000,1022976000,percentused</mem><bw>10240000000000,1024000000,1022976000000,percentused</bw><hostname>hirokivps</hostname><ipaddress>".$instances["public_addresses"]."</ipaddress><password>".$instances["password"]."</password>";}
 }else{echo "<status>error</status><statusmsg>Invalid hash</statusmsg>";}
 }else{echo "<status>error</status><statusmsg>Invalid key</statusmsg>";}
